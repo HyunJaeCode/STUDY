433ab3d162b1674d846592afe6a897d4c84981bb
me <me@DESKTOP-TOVDQ4T>
2022-05-19 22:03:19 +0900

    초기 커밋.

---
+++ STUDY/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Helloworld
+{
+    class Helloworld
+    {
+        static void Main()
+        {
+            double d = 12.13;
+            int i = 1234;
+
+            d = i;
+            Console.WriteLine(d);
+
+            i
+
+
+
+
+        }
+    }
+
+}
